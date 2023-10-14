91a977e0186f4a6eddcf758b24e7342bcf9b850b
pabcrudel <pabcrudel@gmail.com>
2023-10-14 20:07:51 +0200

    Display user data

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,29 @@
 import { createApp, reactive } from "https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js";
 
-const app = reactive({
-  apiUrl: "https://randomuser.me/api/",
-  user: {},
+const user = reactive({
+  email: "",
+  thumbnail: "",
+  photo: "",
+  title: "",
+  firstName: "",
+  lastName: "",
+  username: "",
   async fetchUser() {
-    const response = await fetch(this.apiUrl);
+    const response = await fetch("https://randomuser.me/api/");
     const parsedResponse = await response.json();
 
-    this.user = parsedResponse.results[0];
-    console.log(this.user)
+    const { name, email, picture, login } = parsedResponse.results[0];
+
+    this.email = email;
+    this.thumbnail = picture.thumbnail;
+    this.photo = picture.medium;
+    this.title = name.title;
+    this.firstName = name.first;
+    this.lastName = name.last;
+    this.username = login.username;
   },
 });
 
-app.fetchUser();
+user.fetchUser();
 
-createApp({app}).mount("#app")
+createApp({user}).mount("#app");
