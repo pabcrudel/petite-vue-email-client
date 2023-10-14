12bf1c2bbc0e73525a9a5d4973a9e3bab40a220f
pabcrudel <pabcrudel@gmail.com>
2023-10-14 19:28:08 +0200

    Fetched random user from API

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,17 @@
 import { createApp, reactive } from "https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js";
 
 const app = reactive({
-  greet: "Hello World!"
+  apiUrl: "https://randomuser.me/api/",
+  user: {},
+  async fetchUser() {
+    const response = await fetch(this.apiUrl);
+    const parsedResponse = await response.json();
+
+    this.user = parsedResponse.results[0];
+    console.log(this.user)
+  },
 });
 
-console.log(app.greet)
+app.fetchUser();
 
 createApp({app}).mount("#app")
